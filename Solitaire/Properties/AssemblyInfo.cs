f6675bbeee8dcc7016b7dda5b2c000ab25e7916b
Anthony Daves <adaves1@yahoo.com>
2025-01-06 12:14:41 +0530

    Add files via upload

---
+++ Solitaire/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Solitaire")]
+[assembly: AssemblyProduct("Solitaire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Anthony Daves")]
+[assembly: AssemblyCopyright("Copyright © Anthony Daves 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0ef33d5c-9510-40eb-92fc-03a0111d63ad")]
+
+[assembly: AssemblyVersion("Initial")]
